0bb46c34d8e8945b9be612d83305065f55d0edc0
Natan Felles <natanfelles@gmail.com>
2021-08-09 15:31:27 -0300

    Add Factory functionalities

--- src/Factory.php
+++ src/Factory.php
@@ -16,8 +16,61 @@ namespace Framework\Factories;
  */
 class Factory
 {
-    public function test() : string
+    /**
+     * @var array<string,object>
+     */
+    protected array $classes = [];
+    /**
+     * @var array<string,Factory>
+     */
+    protected static array $factories = [];
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T> $fqcn
+     *
+     * @return T|null
+     */
+    public function get(string $fqcn) : object|null
     {
-        return __METHOD__;
+        return $this->classes[$fqcn] ?? null; // @phpstan-ignore-line
+    }
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T> $fqcn
+     * @param array<int,mixed> $construct
+     *
+     * @return T
+     */
+    public function new(string $fqcn, array $construct = []) : object
+    {
+        return $this->classes[$fqcn] = new $fqcn(...$construct);
+    }
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T> $fqcn
+     * @param array<int,mixed> $construct
+     *
+     * @return T
+     */
+    public function getOrNew(string $fqcn, array $construct = []) : object
+    {
+        return $this->get($fqcn) ?? $this->new($fqcn, $construct);
+    }
+
+    public function set(object $class) : object
+    {
+        return $this->classes[$class::class] = $class;
+    }
+
+    public static function getFactory(string $name = 'default') : Factory
+    {
+        return self::$factories[$name]
+            ?? (self::$factories[$name] = new self());
     }
 }
