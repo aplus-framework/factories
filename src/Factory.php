1510283857b947b821134fb282c76e530f9ae42e
Natan Felles <natanfelles@gmail.com>
2021-08-09 15:59:09 -0300

    Improve PHPDocs

--- src/Factory.php
+++ src/Factory.php
@@ -17,20 +17,26 @@ namespace Framework\Factories;
 class Factory
 {
     /**
+     * All class objects set in the current Factory.
+     *
      * @var array<string,object>
      */
     protected array $classes = [];
     /**
+     * Factory instances.
+     *
      * @var array<string,Factory>
      */
     protected static array $factories = [];
 
     /**
+     * Get an object based in the FQCN.
+     *
      * @template T of object
      *
-     * @param class-string<T> $fqcn
+     * @param class-string<T> $fqcn The Full Qualified Class Name
      *
-     * @return T|null
+     * @return T|null The FQCN object or null if it was not set
      */
     public function get(string $fqcn) : object|null
     {
@@ -38,12 +44,16 @@ class Factory
     }
 
     /**
+     * Create a new object based on the FQCN.
+     *
+     * This method will replace the class instance.
+     *
      * @template T of object
      *
-     * @param class-string<T> $fqcn
-     * @param array<int,mixed> $construct
+     * @param class-string<T> $fqcn The Full Qualified Class Name
+     * @param array<int,mixed> $construct Class constructor arguments
      *
-     * @return T
+     * @return T The created object
      */
     public function new(string $fqcn, array $construct = []) : object
     {
@@ -51,23 +61,40 @@ class Factory
     }
 
     /**
+     * Try to get an existing class instance based on FQCN.
+     * If it is not set, create a new instance and return it.
+     *
      * @template T of object
      *
-     * @param class-string<T> $fqcn
-     * @param array<int,mixed> $construct
+     * @param class-string<T> $fqcn The Full Qualified Class Name
+     * @param array<int,mixed> $construct Class constructor arguments
      *
-     * @return T
+     * @return T The existing or created object
      */
     public function getOrNew(string $fqcn, array $construct = []) : object
     {
         return $this->get($fqcn) ?? $this->new($fqcn, $construct);
     }
 
-    public function set(object $class) : object
+    /**
+     * Set a new object to the list of classes set.
+     *
+     * @param object $object A object
+     *
+     * @return object The same $object
+     */
+    public function set(object $object) : object
     {
-        return $this->classes[$class::class] = $class;
+        return $this->classes[$object::class] = $object;
     }
 
+    /**
+     * Get (existing or created) Factory instance based on a custom name.
+     *
+     * @param string $name The Factory name
+     *
+     * @return Factory The Factory instance
+     */
     public static function getFactory(string $name = 'default') : Factory
     {
         return self::$factories[$name]
